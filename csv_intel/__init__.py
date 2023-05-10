57a6f57220b3a1a02597dbb55d5999f0068f1ab6
Mark Nenadov <marknenadov@gmail.com>
2023-05-09 21:16:50 -0400

    python 3 compatability and some small PEP fixes

--- csv_intel/__init__.py
+++ csv_intel/__init__.py
@@ -1,4 +1,7 @@
-import csv, pprint, sys, os, random
+import csv
+import sys
+import os
+import random
 
 distexplore_enabled = False
 try:
@@ -7,16 +10,17 @@ try:
 except:
 	pass
 
+
 def process(path):
 	f = open(path, 'rb')
 	filename = os.path.basename(path)
-	filename_pure = filename.rsplit('.',1)[0]
-	print filename
+	filename_pure = filename.rsplit('.', 1)[0]
+	print(filename)
 
 	reader = csv.reader(f)
 
 	columns = [x.strip() for x in reader.next()]
-	#print 'Columns:', columns
+	#print( 'Columns:', columns )
 
 	total = 0
 	distribs = {}
@@ -27,11 +31,11 @@ def process(path):
 		total = total + 1
 		populate_distribs_for_columns(cols, columns, distribs)
 
-	print '%d entries' % total
+	print('%d entries' % total)
 	for i,col in enumerate(columns):
 		print_entry_distribs(i, col, distribs, total)
 
-	print 'Numerics:'
+	print('Numerics:')
 	for col in columns:
 		process_numerics_column(col, distribs, total)
 
@@ -41,7 +45,7 @@ def average(seq):
 	return sum(seq)*1.0/len(seq)
 
 def populate_distribs_for_columns(cols, columns, distribs):
-	for i,col in enumerate(cols):
+	for i, col in enumerate(cols):
 		distribs[columns[i]]['value'][col] = distribs[columns[i]]['value'].get(col, 0) + 1
 		if col.strip() == '':
 			distribs[columns[i]]['empty'] += 1
@@ -67,7 +71,7 @@ def process_numerics_column(col, distribs, total):
 				circular=False, freqrep=True)
 
 def print_numeric_value_stats(numeric_values, distribs):
-	print '\t%s: %f - %f (avg: %r, non-0 avg: %r, median: %f, mode: %r)' % (
+	print('\t%s: %f - %f (avg: %r, non-0 avg: %r, median: %f, mode: %r)' % (
 		col,
 		min(numeric_values),
 		max(numeric_values),
@@ -75,11 +79,11 @@ def print_numeric_value_stats(numeric_values, distribs):
 		average([x for x in numeric_values if x != 0]),
 		numeric_values[len(numeric_values)/2],
 		max(distribs[col]['value'].items(), key=lambda x: x[1])[0]
-		)
+		))
 
 
 def print_entry_distribs(i, col, distribs, total):
-	print '\t(%d) %s: %d unique (%.2f%%)\n\t\t%d empty (%.2f%%),\n\t\t%d numeric (%.2f%%)' % (
+	print('\t(%d) %s: %d unique (%.2f%%)\n\t\t%d empty (%.2f%%),\n\t\t%d numeric (%.2f%%)' % (
 		i, col,
 		len(distribs[col]['value']),
 		(len(distribs[col]['value'])*100.0)/total,
@@ -87,15 +91,16 @@ def print_entry_distribs(i, col, distribs, total):
 		(distribs[col]['empty']*100.0) / total,
 		distribs[col]['numeric'],
 		(distribs[col]['numeric']*100.0) / total
-		)
-	print '\t\te.g. %r' % [random.choice(distribs[col]['value'].keys()) for i in xrange(5)]
+		))
+	print('\t\te.g. %r' % [random.choice(distribs[col]['value'].keys()) for i in xrange(5)])
 
 def run(args):
 	if len(args) < 2 or (len(args) >= 2 and args[1] in ['help', '-h', '--help']):
-		print 'Usage:'
-		print '\tcsv-intel data.csv'
+		print('Usage:')
+		print('\tcsv-intel data.csv')
 	else:
 		process(args[1])
+
 
 if __name__ == '__main__':
 	run(sys.argv)
